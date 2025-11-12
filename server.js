06504a146694208b4c7cc77b9e90101ef7f8f659
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-12 10:51:13 +0100

    feat: map

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 'use strict';
-console.log('ciao');
+
 const students = ['Anna', 'Sara', 'Paolo'];
-//map
+const numeri = [1,3,4,6,7,8,11];
+//forEach
 const user = (array) => {
     array.forEach((element, index) => {
-        console.log(element, index);
+        console.log(`studente di nome ${element}, con il codice ${index}`);
     });
 } 
 //richiamo la funzione
 user(students);
+
+//map
+const multipli = (array) => {
+    return array.map((element) => element*element);
+}
+console.log(multipli(numeri));
